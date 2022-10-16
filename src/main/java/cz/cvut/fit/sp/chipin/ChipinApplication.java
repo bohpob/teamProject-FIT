6296629f41577fe5315c23d49b35fcfe2157f36e
popovle1 <popovle1@fit.cvut.cz>
2022-10-16 14:35:14 +0200

    Initial commit

---
+++ src/main/java/cz/cvut/fit/sp/chipin/ChipinApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.sp.chipin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChipinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChipinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/cvut/fit/sp/chipin/ChipinApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.sp.chipin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChipinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
