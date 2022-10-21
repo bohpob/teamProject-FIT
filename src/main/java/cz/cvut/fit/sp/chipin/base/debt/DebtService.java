aa2fa86f21133b3b839a57a305770f94c5707380
popovle1 <popovle1@fit.cvut.cz>
2022-10-21 17:59:29 +0200

    Created base for DebtService class

--- src/main/java/cz/cvut/fit/sp/chipin/base/debt/DebtService.java
+++ src/main/java/cz/cvut/fit/sp/chipin/base/debt/DebtService.java
@@ -1,4 +1,26 @@
 package cz.cvut.fit.sp.chipin.base.debt;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
 public class DebtService {
+
+    public void recalculate() throws Exception {
+    // TODO:
+    }
+
+    public List<DebtDTO> getAllFromUser() throws Exception {
+        // TODO:
+        return null;
+    }
+
+    public List<DebtDTO> getAllFromUserInGroup() throws Exception {
+        // TODO:
+        return null;
+    }
+
 }
