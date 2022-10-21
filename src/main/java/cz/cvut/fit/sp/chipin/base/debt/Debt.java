bed7ff37bb10841bd319d7a2ea0106f9165e9f5b
popovle1 <popovle1@fit.cvut.cz>
2022-10-21 17:37:45 +0200

    Created base for DebtDTO class

--- src/main/java/cz/cvut/fit/sp/chipin/base/debt/Debt.java
+++ src/main/java/cz/cvut/fit/sp/chipin/base/debt/Debt.java
@@ -1,4 +1,39 @@
 package cz.cvut.fit.sp.chipin.base.debt;
 
+import cz.cvut.fit.sp.chipin.authentication.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "debt")
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter
+@Setter
 public class Debt {
+    @Id
+    @SequenceGenerator(
+            name = "debt_sequence",
+            sequenceName = "debt_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "debt_sequence"
+    )
+    private Integer id;
+
+    @ManyToOne()
+    @JoinColumn(name = "lender_id")
+    private User lender;
+
+    @ManyToOne()
+    @JoinColumn(name = "borrower_id")
+    private User borrower;
+
+    @NotBlank
+    @Column(name = "amount", nullable = false)
+    private float amount;
 }
--- src/main/java/cz/cvut/fit/sp/chipin/base/debt/DebtDTO.java
+++ src/main/java/cz/cvut/fit/sp/chipin/base/debt/DebtDTO.java
@@ -1,4 +1,14 @@
 package cz.cvut.fit.sp.chipin.base.debt;
 
+import cz.cvut.fit.sp.chipin.authentication.user.UserDTO;
+
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
 public class DebtDTO {
+    private final Integer id;
+    private UserDTO lender;
+    private UserDTO borrower;
+    private Integer amount;
 }
