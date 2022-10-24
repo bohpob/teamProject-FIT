412a5ef041837a62d0f7833455ea3b7af4e558be
poberboh <poberboh@fit.cvut.cz>
2022-10-24 23:37:18 +0200

    Created base for Transaction class

---
+++ src/main/java/cz/cvut/fit/sp/chipin/base/transaction/Transaction.java
@@ -0,0 +1,36 @@
+package cz.cvut.fit.sp.chipin.base.transaction;
+
+import cz.cvut.fit.sp.chipin.authentication.user.User;
+import cz.cvut.fit.sp.chipin.base.group.Group;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "transaction")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Transaction {
+    @Id
+    @SequenceGenerator(
+            name = "transaction_sequence",
+            sequenceName = "transaction_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "transaction_sequence"
+    )
+    private Long id;
+    private String name;
+    private Float amount;
+
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
