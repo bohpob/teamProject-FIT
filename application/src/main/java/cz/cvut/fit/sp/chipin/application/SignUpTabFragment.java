40fa9a2a06fd7a5cf0a9d08401fecaaa05c78b2e
Baibatchinov Alisher <baibatchinova@gmail.com>
2022-10-28 22:39:30 +0200

    Registration page customization

--- application/src/main/java/cz/cvut/fit/sp/chipin/application/SignUpTabFragment.java
+++ application/src/main/java/cz/cvut/fit/sp/chipin/application/SignUpTabFragment.java
@@ -12,6 +12,8 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import org.jetbrains.annotations.NotNull;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -20,9 +22,12 @@ import retrofit2.Response;
 public class SignUpTabFragment extends Fragment {
 
     TextView signupText;
-    EditText name;
-    EditText email;
-    EditText password;
+    TextInputEditText name;
+    TextInputEditText email;
+    TextInputEditText password;
+    TextInputLayout name_layout;
+    TextInputLayout email_layout;
+    TextInputLayout password_layout;
     Button signUp;
     float v = 0;
 
@@ -33,26 +38,29 @@ public class SignUpTabFragment extends Fragment {
 
         signupText = root.findViewById(R.id.signupText);
         name = root.findViewById(R.id.name_register);
+        name_layout = root.findViewById(R.id.name_register_outer);
         email = root.findViewById(R.id.email_register);
+        email_layout = root.findViewById(R.id.email_register_outer);
         password = root.findViewById(R.id.password_register);
+        password_layout = root.findViewById(R.id.password_register_outer);
         signUp = root.findViewById(R.id.signupButton);
 
         signupText.setTranslationX(800);
-        name.setTranslationX(800);
-        email.setTranslationX(800);
-        password.setTranslationX(800);
+        name_layout.setTranslationX(800);
+        email_layout.setTranslationX(800);
+        password_layout.setTranslationX(800);
         signUp.setTranslationX(800);
 
         signupText.setAlpha(v);
-        name.setAlpha(v);
-        email.setAlpha(v);
-        password.setAlpha(v);
+        name_layout.setAlpha(v);
+        email_layout.setAlpha(v);
+        password_layout.setAlpha(v);
         signUp.setAlpha(v);
 
         signupText.animate().translationX(0).alpha(1).setDuration(1000).setStartDelay(100).start();
-        name.animate().translationX(0).alpha(1).setDuration(1000).setStartDelay(300).start();
-        email.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(500).start();
-        password.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(700).start();
+        name_layout.animate().translationX(0).alpha(1).setDuration(1000).setStartDelay(300).start();
+        email_layout.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(500).start();
+        password_layout.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(700).start();
         signUp.animate().translationX(0).alpha(1).setDuration(800).setStartDelay(900).start();
 
         signUp.setOnClickListener(new View.OnClickListener() {
@@ -84,8 +92,6 @@ public class SignUpTabFragment extends Fragment {
                 String message;
                 if (response.isSuccessful()){
                     message = "Successful...";
-//                    viewPager.setCurrentItem(0);
-//                    finish();
                 } else {
                     message = "Ann error occurred, please try again later...";
                 }
