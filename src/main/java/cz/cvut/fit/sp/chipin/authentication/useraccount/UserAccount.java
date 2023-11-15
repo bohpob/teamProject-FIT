96b385341e52dfe93138e03beaaa744c3d994921
Artem Shitin <shitiart@fit.cvut.cz>
2023-11-15 23:26:24 +0100

    Optimized gradle and configuration files

--- src/main/java/cz/cvut/fit/sp/chipin/authentication/useraccount/UserAccount.java
+++ src/main/java/cz/cvut/fit/sp/chipin/authentication/useraccount/UserAccount.java
@@ -14,10 +14,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 @Entity
 @Table(name = "user_account")
