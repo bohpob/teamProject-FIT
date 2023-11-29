2abf6eb932903a29faa59bde7fa80ea115816e60
Artem Shitin <shitiart@fit.cvut.cz>
2023-11-29 16:05:42 +0100

    Revert "Merge branch '46-add-spring-authorization-server' into 'master'"
    This reverts merge request !47

--- src/main/java/cz/cvut/fit/sp/chipin/base/amount/AmountRepository.java
+++
@@ -1,9 +0,0 @@
-package cz.cvut.fit.sp.chipin.base.amount;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AmountRepository extends JpaRepository<Amount, AmountKey> {
-    void deleteAmountsByTransactionId(Long transactionId);
-}
