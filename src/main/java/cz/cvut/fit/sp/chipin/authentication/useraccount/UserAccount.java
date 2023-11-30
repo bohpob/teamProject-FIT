acb5e8bfa764009ed7cad1864552ae4f1ac70a74
Nikita Nikolchuk <nikolnik@fit.cvut.cz>
2023-11-30 15:17:56 +0100

    Update UserAccount for Keycloak usage

--- src/main/java/cz/cvut/fit/sp/chipin/authentication/useraccount/UserAccount.java
+++ src/main/java/cz/cvut/fit/sp/chipin/authentication/useraccount/UserAccount.java
@@ -5,31 +5,28 @@ import cz.cvut.fit.sp.chipin.base.debt.Debt;
 import cz.cvut.fit.sp.chipin.base.member.Member;
 import cz.cvut.fit.sp.chipin.base.transaction.Transaction;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 @Entity
-
 @Table(name = "user_account")
 @NoArgsConstructor
 @Getter
 @Setter
-public class UserAccount implements UserDetails {
+public class UserAccount {
 
     @Id
-    @Column(length = 36)
+    @Column(name = "user_entity_id")
     private String id;
+
+    @MapsId
+    @OneToOne
+    @JoinColumn(name = "user_entity_id")
+    private UserEntity userEntity;
 
     @OneToMany(mappedBy = "userAccount")
     private List<Member> members = new ArrayList<>();
@@ -46,68 +43,12 @@ public class UserAccount implements UserDetails {
     @OneToMany(mappedBy = "payer")
     private List<Transaction> transactions = new ArrayList<>();
 
-    @NotBlank
-    @Column(name = "name")
-    private String name;
-    @Email
-    @Column(name = "email", nullable = false)
-    private String email;
-    @NotBlank
-    @Column(name = "password", nullable = false)
-    private String password;
-    private Boolean locked = false;
-    private Boolean enabled = false;
-
-    public UserAccount(String name, String email) {
-        this.name = name;
-        this.email = email;
+    public UserAccount(UserEntity userEntity) {
+        this.userEntity = userEntity;
     }
 
-    public UserAccount(String name, String email, String password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return getUsername();
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        SimpleGrantedAuthority authority = new SimpleGrantedAuthority("USER");
-        return Collections.singletonList(authority);
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return !locked;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return enabled;
+    public String getName() {
+        return userEntity.getFirstName() + " " + userEntity.getLastName();
     }
 
     public void addMembership(Member member) {
--- src/main/java/cz/cvut/fit/sp/chipin/authentication/useraccount/UserAccountRepository.java
+++ src/main/java/cz/cvut/fit/sp/chipin/authentication/useraccount/UserAccountRepository.java
@@ -1,26 +1,10 @@
 package cz.cvut.fit.sp.chipin.authentication.useraccount;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
 public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
-    //    Optional<UserAccount> findUserAccountByUserAccountname(String userAccountname);
-    Optional<UserAccount> findUserAccountByEmail(String email);
-
-
-
-    @Transactional
-    @Modifying
-    @Query(
-            "UPDATE UserAccount a " + "SET a.enabled = TRUE where a.email = ?1"
-    )
-    int enableUserAccount(String email);
-
 }
