74c4bc2c601e7c53843cc7337f9c9cf695de20ad
Nikita Nikolchuk <nikolnik@fit.cvut.cz>
2023-12-01 18:22:05 +0100

    Add User, Debt, Log and Member mappers

---
+++ src/main/java/cz/cvut/fit/sp/chipin/base/log/mapper/LogMapper.java
@@ -0,0 +1,11 @@
+package cz.cvut.fit.sp.chipin.base.log.mapper;
+
+import cz.cvut.fit.sp.chipin.base.log.Log;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface LogMapper {
+    @Mapping(source = "userAccount.userEntity.firstName", target = "userName")
+    LogReadLogResponse entityToReadLogResponse(Log log);
+}
