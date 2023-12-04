4d66c904c5c3914a6d7036bae0fe8171a6cf0a44
Bohdan Poberezhnyi <poberboh@fit.cvut.cz>
2023-12-04 03:54:23 +0100

    implemented a calculator for calculating amounts

---
+++ src/main/java/cz/cvut/fit/sp/chipin/base/amount/calculator/PercentBasedAmountCalculator.java
@@ -0,0 +1,33 @@
+package cz.cvut.fit.sp.chipin.base.amount.calculator;
+
+import cz.cvut.fit.sp.chipin.authentication.useraccount.UserAccount;
+import cz.cvut.fit.sp.chipin.base.amount.Amount;
+import cz.cvut.fit.sp.chipin.base.transaction.Transaction;
+import cz.cvut.fit.sp.chipin.base.transaction.spender.MemberAbstractRequest;
+import cz.cvut.fit.sp.chipin.base.transaction.spender.PercentTransactionMember;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PercentBasedAmountCalculator implements AmountCalculator {
+    @Override
+    public List<Amount> calculateAmounts(List<UserAccount> users, Transaction transaction, List<MemberAbstractRequest> spenders) throws Exception {
+        List<Amount> amounts = new ArrayList<>();
+
+        float totalPercent = 0f;
+        for (int i = 0; i < users.size(); i++) {
+            try {
+                PercentTransactionMember memberByPercentages = (PercentTransactionMember) spenders.get(i);
+                float percent = memberByPercentages.getPercentage();
+                amounts.add(new Amount(users.get(i), transaction, percent * transaction.getAmount() / 100));
+                totalPercent += percent;
+            } catch (Exception e) {
+                throw new Exception("UserAccount not found.");
+            }
+        }
+
+        if (Math.abs(totalPercent - 100) > 0.01)
+            throw new Exception("The total percentage is not 100");
+        return amounts;
+    }
+}
