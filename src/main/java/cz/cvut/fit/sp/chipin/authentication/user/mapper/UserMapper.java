a550ca06c92e33c14770977b3163ee18d0732246
Anton Ovchinnikov <ovchiant@fit.cvut.cz>
2023-12-09 17:09:04 +0100

    Warning resolved

--- src/main/java/cz/cvut/fit/sp/chipin/authentication/user/mapper/UserMapper.java
+++ src/main/java/cz/cvut/fit/sp/chipin/authentication/user/mapper/UserMapper.java
@@ -1,10 +1,11 @@
 package cz.cvut.fit.sp.chipin.authentication.user.mapper;
 
 import cz.cvut.fit.sp.chipin.authentication.user.User;
+import cz.cvut.fit.sp.chipin.base.transaction.mapper.TransactionMapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {TransactionMapper.class})
 public interface UserMapper {
     @Mapping(source = "firstName", target = "name")
     @Mapping(source = "email", target = "email")
