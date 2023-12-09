12bbfc0a6512e31a9dcd7bee3a8cf895fb57871f
Artem Shitin <shitiart@fit.cvut.cz>
2023-12-09 04:23:43 +0100

    Return the same keycloak docker-compose as it was, added new insert script

--- user_entity_insert.sql
+++ user_entity_insert.sql
@@ -1,5 +1,12 @@
 INSERT INTO user_entity (id, email, email_constraint, email_verified, enabled, federation_link, first_name, last_name, realm_id, username, created_timestamp, service_account_client_link, not_before)
 VALUES
-    ('ba0a07c8-7f66-4cee-9f81-2d3a985d137d', 'tajil52467@newcupon.com', 'tajil52467@newcupon.com', true, true, NULL, '1', '1', 'dc66e9b7-4572-4419-ba5c-68a5e5c6fa01', 'tajil52467@newcupon.com', 1701701767746, NULL, 0),
-    ('94c68919-8adc-46fc-ae47-71ea197a1b48', 'rokahe4622@lanxi8.com', 'rokahe4622@lanxi8.com', true, true, NULL, '2', '2', 'dc66e9b7-4572-4419-ba5c-68a5e5c6fa01', 'rokahe4622@lanxi8.com', 1701701990122, NULL, 0),
-    ('1599ab4d-8bc8-468d-a08b-92696a24b48d', 'bosonix477@mcenb.com', 'bosonix477@mcenb.com', true, true, NULL, '3', '3', 'dc66e9b7-4572-4419-ba5c-68a5e5c6fa01', 'bosonix477@mcenb.com', 1701702067402, NULL, 0);
+    ('086dda9e-c40c-4c53-aac9-14ae5ba3bef2', 'a@a.aa', 'a@a.aa', true, true, NULL, 'a', 'a', 'dc66e9b7-4572-4419-ba5c-68a5e5c6fa01', 'a@a.aa', 1701892268196, NULL, 0),
+    ('189c63b8-010c-4ab1-85e5-93bf9da4a258', 'b@b.bb', 'b@b.bb', true, true, NULL, 'b', 'b', 'dc66e9b7-4572-4419-ba5c-68a5e5c6fa01', 'b@b.bb', 1701892308152, NULL, 0),
+    ('077966c2-9fe7-4073-b1e6-aeb3879db493', 'c@c.cc', 'c@c.cc', true, true, NULL, 'c', 'c', 'dc66e9b7-4572-4419-ba5c-68a5e5c6fa01', 'c@c.cc', 1701892318354, NULL, 0),
+    ('e2291b4d-0db0-418c-b9b7-f530f4a74484', 'd@d.dd', 'd@d.dd', true, true, NULL, 'd', 'd', 'dc66e9b7-4572-4419-ba5c-68a5e5c6fa01', 'd@d.dd', 1701892328746, NULL, 0),
+    ('82a54b53-7cce-4818-9b35-923526c8a138', 'e@e.ee', 'e@e.ee', true, true, NULL, 'e', 'e', 'dc66e9b7-4572-4419-ba5c-68a5e5c6fa01', 'e@e.ee', 1701892391333, NULL, 0),
+    ('6a901c84-ece7-4bfa-aaa5-951e84857644', 'f@f.ff', 'f@f.ff', true, true, NULL, 'f', 'f', 'dc66e9b7-4572-4419-ba5c-68a5e5c6fa01', 'f@f.ff', 1701892402641, NULL, 0),
+    ('018c02e1-2d4e-4c2a-8c27-2be3e225628a', 'g@g.gg', 'g@g.gg', true, true, NULL, 'g', 'g', 'dc66e9b7-4572-4419-ba5c-68a5e5c6fa01', 'g@g.gg', 1701892412234, NULL, 0),
+    ('d070208b-5071-4052-a531-26d0023e5299', 'h@h.hh', 'h@h.hh', true, true, NULL, 'h', 'h', 'dc66e9b7-4572-4419-ba5c-68a5e5c6fa01', 'h@h.hh', 1701892429303, NULL, 0),
+    ('b63ac93c-f8bf-42d7-8728-c63fa8646742', 'i@i.ii', 'i@i.ii', true, true, NULL, 'i', 'i', 'dc66e9b7-4572-4419-ba5c-68a5e5c6fa01', 'i@i.ii', 1701892437782, NULL, 0),
+    ('a1a81cae-2c73-41ff-b96f-8d4e81b0058b', 'j@j.jj', 'j@j.jj', true, true, NULL, 'j', 'j', 'dc66e9b7-4572-4419-ba5c-68a5e5c6fa01', 'j@j.jj', 1701892459222, NULL, 0);
