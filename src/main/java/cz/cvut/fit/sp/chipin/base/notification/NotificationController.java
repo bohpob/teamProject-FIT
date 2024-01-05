48992cde9c226abb059fe0aeff0e0a700080511b
Bohdan Poberezhnyi <poberboh@fit.cvut.cz>
2024-01-05 15:12:41 +0100

    me

--- src/main/java/cz/cvut/fit/sp/chipin/base/notification/NotificationController.java
+++ src/main/java/cz/cvut/fit/sp/chipin/base/notification/NotificationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 
 @RestController
-@RequestMapping("/api/v1/notifications")
+@RequestMapping("/api/v1/me/notifications")
 @AllArgsConstructor
 public class NotificationController {
     private final NotificationService notificationService;
